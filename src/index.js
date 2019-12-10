82e9db8aa7a46d7f12d710f035bc8a8e611574b1
stvik <stvik2@gmail.com>
2019-12-10 15:16:03 -0500

    finished-basic-deliv

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let formEl = document.querySelector("form") 
+  
+  formEl.addEventListener("submit", submitForm)
+  
 });
+
+function submitForm(event) {
+	event.preventDefault()
+	// console.log("Submit???")
+	let description = document.getElementById("new-task-description").value
+	addTask(description)
+	event.target.reset()
+}
+
+function addTask(description) {
+	let ul = document.getElementById("tasks")
+
+	let newLi = document.createElement("li")
+	newLi.innerText = description
+
+	ul.append(newLi)
+}

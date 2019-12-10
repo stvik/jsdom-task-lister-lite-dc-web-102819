b18fa12df4bdc658a0a622025385515a14a1363f
stvik <stvik2@gmail.com>
2019-12-10 16:14:42 -0500

    delete button and color working

--- src/index.js
+++ src/index.js
@@ -9,25 +9,36 @@ function submitForm(event) {
 	event.preventDefault()
 	// console.log("Submit???")
 	let description = document.getElementById("new-task-description").value
-	addTask(description)
+	let priority = document.getElementById("priority").value
+	addTask(description, priority)
 	
-	event.target.reset()	
+	event.target.reset()
 }
 
-function addTask(description) {
+let i = 0
+function addTask(description, priority) {
+
+
 	let ul = document.getElementById("tasks")
 
 	let newLi = document.createElement("li")
 	newLi.innerText = description
+	i++
+	newLi.setAttribute("id", `${description}-${i}-task`)
+	newLi.style.color = priority
 
 	ul.append(newLi)
 	let makeButton = document.createElement("BUTTON");
 	makeButton.innerHTML = "x";
-	makeButton.setAttribute("id", `${description}`)
+	makeButton.setAttribute("id", `${description}-${i}`)
 	newLi.appendChild(makeButton)
+	makeButton.addEventListener("click", deleteTask)
 }
 
 function deleteTask(task) {
 
+ taskId = task.target.id
+ let deleteEl = document.getElementById(`${taskId}-task`)
+ deleteEl.remove();
 }
 
